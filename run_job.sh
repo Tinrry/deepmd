ed0dc4e33fa321e79cf90dc3feebe02a45801dfd
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-11-15 11:33:18 +0800

    remove record run time,because in sbatch is usless.

--- run_job.sh
+++ run_job.sh
@@ -8,17 +8,12 @@
 #SBATCH --mem=0
 #SBATCH --partition=operation
 
-
 # 初始化 conda 并激活 conda 环境 
 source ~/.bashrc
 conda activate ~/deepmd2.2.11-cpu
 
 # 堆栈大小设置为无限制
 ulimit -s unlimited
-
-# get the current time
-echo "Job start time: $(date)"
-start=$(date +%s)
 
 # ------- train -------
 # srun --partition=operation dp train input.json
@@ -27,13 +22,5 @@ start=$(date +%s)
 # ------- train end -------
 
 # get the input from the command line
-
-# todo  srun mpirun -np 1 python run.py   this is not working       
+# todo  error:srun mpirun -np 1 python run.py   this is not working       
 srun python run.py $1 $2 $3 ${SLURM_JOB_ID}_predict.txt
-
-# get the current time
-echo "Job end time: $(date)"
-end=$(date +%s)
-
-duration=end-start
-echo "Job duration: $duration seconds" >> ${SLURM_JOB_ID}_predict.txt
