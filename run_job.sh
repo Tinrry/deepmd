d186a0f719abcaf3f4e79c486a8c575a6c419b08
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-11-15 11:19:35 +0800

    ${SLURM_JOB_ID}

--- run_job.sh
+++ run_job.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=deepmd_zhh
 #SBATCH --nodes=1 
-#SBATCH --ntasks=56   # the node in CPUTot=56
+#SBATCH --ntasks=1   # the node in CPUTot=56, error: this will cause pending
 #SBATCH --time=1-02:00:00
 #SBATCH --output=%J.out
 #SBATCH --error=%J.err
 #SBATCH --mem=0
 #SBATCH --partition=operation
-#SBATCH --account=material_1
 
 
 # 初始化 conda 并激活 conda 环境 
 source ~/.bashrc
-conda activate deepmd2.2.11-cpu
+conda activate ~/deepmd2.2.11-cpu
 
 # 堆栈大小设置为无限制
 ulimit -s unlimited
@@ -29,7 +28,7 @@ echo "Job start time: $(date)"
 # get the input from the command line
 
 # todo  srun mpirun -np 1 python run.py   this is not working       
-srun python run.py $1 $2 $3 %J_predict.txt
+srun python run.py $1 $2 $3 ${SLURM_JOB_ID}_predict.txt
 
 # get the current time
 echo "Job end time: $(date)"
